e1fe4f7cb383bd620abf7334d60cc7dbdc063397
aluno <aluno@ifpe>
2022-09-29 09:30:01 -0300

    primeiros arquivos do projeto

---
+++ src/main/java/br/com/ifpe/oxefoodwarley/OxefoodWarleyApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefoodwarley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodWarleyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodWarleyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifpe/oxefoodwarley/OxefoodWarleyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefoodwarley;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OxefoodWarleyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
